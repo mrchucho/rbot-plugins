f5f99ef7d55e10a5ae31cab0613a5db9768d05e0
mrchucho <mrchucho@mrchucho.net>
2012-03-09 10:30:28 -0600

    Added "Fake DB" if sqlite is not configured. Re-formatted code.

--- urls.rb
+++ urls.rb
@@ -8,6 +8,13 @@ require 'cgi'
 require 'uri'
 require 'date'
 
+class FakeDB
+  attr_accessor :results_as_hash
+  def table_info(table); return {} ; end
+  def execute_batch(sql); end;
+  def execute(sql); end;
+end
+
 =begin
 Notes:
     1. need to make sure I limit the number of responses, use BotConfig 'max'
@@ -43,18 +50,20 @@ class UrlsPlugin < Plugin
                                                  :validate => Proc.new{|v| v>0 and v<13},
                                                  :desc => "Number of URL Search Results to return.")
     def initialize
-        super
-	begin
+      super
+      begin
         @db = SQLite3::Database.new(DATABASE)
         @db.results_as_hash = true
 
         #@db.create_function('date_only',1) do |ctx,dt|
-            #DateTime.parse(dt).strftime(DATETIME)
+        #DateTime.parse(dt).strftime(DATETIME)
         #end
-	rescue => e
-	puts "Error opening database: #{e}"
-	end
+      rescue => e
+        @db = FakeDB.new
+        puts "Error opening database: #{e}"
+      end
     end
+
     def create_database
         if @db.table_info("urls").empty? then
             puts "Database does not exist, creating"
@@ -76,73 +85,73 @@ EOF
     end
 
     def help(plugin,topic="")
-        "All your URLs belong to us!"
+      "All your URLs belong to us!"
     end
+
     def listen(m)
-        return unless m.kind_of?(PrivMessage)
-        return if m.address?
+      return unless m.kind_of?(PrivMessage)
+      return if m.address?
 
-        if m.message =~ /(f|ht)tps?:\/\//
-          if m.message =~ /((f|ht)tps?:\/\/.*?)(?:\s+|$)/
-            urlstr = $1
-            title = find_title(urlstr)
-            inform_title(m,urlstr,title) if title
-            save_url(m,urlstr,title)
-          end
+      if m.message =~ /(f|ht)tps?:\/\//
+        if m.message =~ /((f|ht)tps?:\/\/.*?)(?:\s+|$)/
+          urlstr = $1
+        title = find_title(urlstr)
+        inform_title(m,urlstr,title) if title
+        save_url(m,urlstr,title)
         end
+      end
     end
-    
-    def search(m, parameters)
-        results = Array.new
-        
-        # in case they're still using
-        # the "urls search" syntax, remove the first "search"
-        params = parameters[:query]
-        params.shift if (params[0] == "search" && params.size>1)
 
-	maxurls = @bot.config['murls.max_results']
-        if params[-1].to_i > 0 then
-	    wants = params.pop.to_i
-            limit = (wants <= maxurls) ? wants : maxurls
-        else
-            limit = maxurls
-        end
-        query = params.join(' ').downcase
-        if query.to_i > 0 then
-	    wants = query.to_i
-            @db.execute("""
+    def search(m, parameters)
+      results = Array.new
+      # in case they're still using
+      # the "urls search" syntax, remove the first "search"
+      params = parameters[:query]
+      params.shift if (params[0] == "search" && params.size>1)
+
+      maxurls = @bot.config['murls.max_results']
+      if params[-1].to_i > 0 then
+        wants = params.pop.to_i
+        limit = (wants <= maxurls) ? wants : maxurls
+      else
+        limit = maxurls
+      end
+      query = params.join(' ').downcase
+      if query.to_i > 0 then
+        wants = query.to_i
+        @db.execute("""
                 select distinct url,seen_at,submitted_by
                 from urls
                 order by seen_at desc
                 limit ?""",
-                (wants <= maxurls) ? wants : maxurls){|row| results.push row_to_message(row)}
-        elsif is_member(m,query) then
-            @db.execute("""
+                  (wants <= maxurls) ? wants : maxurls){|row| results.push row_to_message(row)}
+      elsif is_member(m,query) then
+        @db.execute("""
                 select distinct url,seen_at,submitted_by
                 from urls
                 where lower(submitted_by) = lower(?)
                 order by seen_at desc
                 limit ?""",
-                query,limit){|row| results.push row_to_message(row)}
-        elsif query == 'yesterday'
-            @db.execute("""
+                  query,limit){|row| results.push row_to_message(row)}
+      elsif query == 'yesterday'
+        @db.execute("""
                 select distinct url,seen_at,submitted_by
                 from urls
                 where strftime('%Y%m%d',seen_at)=strftime('%Y%m%d','now','-1 days')
                 order by seen_at desc
                 limit ?""",
-                limit){|row| results.push row_to_message(row)}
-        elsif query == 'today'
-            @db.execute("""
+                  limit){|row| results.push row_to_message(row)}
+      elsif query == 'today'
+        @db.execute("""
                 select distinct url,seen_at,submitted_by
                 from urls
                 where strftime('%Y%m%d',seen_at)=strftime('%Y%m%d','now')
                 order by seen_at desc
                 limit ?""",
-                limit){|row| results.push row_to_message(row)}
-        elsif query == 'tomorrow'
-            results.push "I cannot predict the future..."
-        else 
+                  limit){|row| results.push row_to_message(row)}
+      elsif query == 'tomorrow'
+        results.push "I cannot predict the future..."
+      else
 =begin
             # hrm, not sure. it seems like you should probably *always* search
             # the hostname, too
@@ -165,26 +174,26 @@ EOF
                     limit){|row| results.push row}
             end
 =end
-                @db.execute("""
+        @db.execute("""
                     select distinct url,seen_at,submitted_by
                     from urls
                     where (lower(host) like '%#{query}%' or
                             lower(title) like '%#{query}%')
                     order by seen_at desc
                     limit ?""",
-                    limit){|row| results.push row_to_message(row)}
-        end
+                      limit){|row| results.push row_to_message(row)}
+      end
 
-        if results.empty?
-            m.reply "No matching URLs found."
-        else
-            results.each{|res| m.reply res}
-        end
+      if results.empty?
+        m.reply "No matching URLs found."
+      else
+        results.each{|res| m.reply res}
+      end
     end
 
     def stats(m,params)
-        m.reply "-=[ Today ]=-"
-        @db.execute("""
+      m.reply "-=[ Today ]=-"
+      @db.execute("""
             select submitted_by,count(url)
             from urls
             where strftime('%Y%m%d',seen_at) = strftime('%Y%m%d','now')
@@ -197,41 +206,41 @@ EOF
             from (select distinct submitted_by,url from urls)
             group by submitted_by
             order by count(url) desc
-	    limit 10"""){|row| m.reply "#{row[0]} => #{row[1]}"}
+            limit 10"""){|row| m.reply "#{row[0]} => #{row[1]}"}
     end
 
     def find_title(urlstr)
-        pagedata = @bot.httputil.get(URI.parse(urlstr))
-        return unless TITLE_RE.match(pagedata)
-        title = $1.strip.gsub(/\s*\n+\s*/, " ")
-        title = CGI::unescapeHTML title
-        # title = title[0..255] if title.length > 255
-        return title
+      pagedata = @bot.httputil.get(URI.parse(urlstr))
+      return unless TITLE_RE.match(pagedata)
+      title = $1.strip.gsub(/\s*\n+\s*/, " ")
+      title = CGI::unescapeHTML title
+      # title = title[0..255] if title.length > 255
+      return title
     end
 
     def inform_title(m,urlstr,title)
-      m.reply "13#{title} 14-=[ #{urlstr} ]=-" 
+      m.reply "13#{title} 14-=[ #{urlstr} ]=-"
     end
 
     def save_url(m,urlstr,title)
-        url = URI.parse(urlstr)
-        begin
-            @db.execute("insert into urls(url,host,title,seen_at,submitted_by,channel) values(?,?,?,datetime('now'),?,?)",
-                        urlstr,url.host,title,m.sourcenick,m.channel)
-        rescue => err
-            m.reply "Error: #{err}"
-        end
+      url = URI.parse(urlstr)
+      begin
+        @db.execute("insert into urls(url,host,title,seen_at,submitted_by,channel) values(?,?,?,datetime('now'),?,?)",
+                    urlstr,url.host,title,m.sourcenick,m.channel)
+      rescue => err
+        m.reply "Error: #{err}"
+      end
     end
 
     def is_member(m,query)
-        return false if not m.channel
-        #q = query.downcase
-        #return @bot.channels[m.channel].users.keys.find {|u| u.downcase == q}
-    	return m.channel.has_user?(query)
+      return false if not m.channel
+      #q = query.downcase
+      #return @bot.channels[m.channel].users.keys.find {|u| u.downcase == q}
+      return m.channel.has_user?(query)
     end
 
     def row_to_message(row)
-        "#{row['url']} [#{DateTime.parse(row['seen_at']).strftime(DATETIME_FORMAT)}] <#{row['submitted_by']}>"
+      "#{row['url']} [#{DateTime.parse(row['seen_at']).strftime(DATETIME_FORMAT)}] <#{row['submitted_by']}>"
     end
 end
 
